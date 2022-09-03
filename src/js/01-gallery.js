68d4aa7731bc70834c82f5335ad2287b87fdfacd
Oleksandr-Nehoda <iamsoul@ukr.net>
2022-09-03 17:39:48 +0300

    task-01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,39 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+//  Імпорт бібліотеки
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryConteinerRef = document.querySelector('.gallery');
+const galleryMarkup = createGalleryCards(galleryItems);
+
+
+galleryConteinerRef.insertAdjacentHTML('beforeend', galleryMarkup);
+
+galleryConteinerRef.addEventListener('click', onOpenLightbox);
+
+function onOpenLightbox(event) {
+
+    event.preventDefault();
+
+    if (event.target.nodeName !== 'IMG'){
+        return
+    }
+    var lightbox = new SimpleLightbox('.gallery a', { 
+        captionsData: 'alt',
+        captionDelay: '250',
+ });
+}
+
+function createGalleryCards (galleryItems) {
+    return galleryItems.map( ({preview, original, description}) => {
+    return `
+    <a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>`
+    }).join("");
+}
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,30 @@
 
+import Player from '@vimeo/player';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('play', function() {
+    console.log('played the video!');
+});
+
+
+const onPlay = function(data) {
+   console.log(data)
+};
+
+player.on('timeupdate', onPlay);
+
+player.setCurrentTime(40.000).then(function(seconds) {
+    seconds = 40.000;
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
